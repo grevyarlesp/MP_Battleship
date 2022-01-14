a73046430129b8e21a626d44c0c224877fcbfb56
grevyarlesp <tr4nquoc@gmail.com>
2022-01-14 20:54:19 +0700

    change server ip

--- server/main.py
+++ server/main.py
@@ -3,7 +3,7 @@ from ClientThread import ClientThread
 from Server import Server
 
 PORT = 55342
-HOST = '127.0.0.1'
+HOST = '0.0.0.0'
 
 def main():
     serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
